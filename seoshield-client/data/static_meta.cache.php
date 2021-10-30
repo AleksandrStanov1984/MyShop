664a917f728becdd55531e7c1c54b5c33c4d2d6c
Aleksandr <aleksstanov1984@gmail.com>
2021-10-30 17:36:02 +0300

    my commit

---
+++ seoshield-client/data/static_meta.cache.php
@@ -0,0 +1,90 @@
+<?php return array (
+  '//sz.ua/' => 
+  array (
+    0 => 'SZ.UA - интернет магазин цифровой и бытовой техники и электроники, выгодные цены и большой каталог в Украине и Киеве',
+    1 => '',
+    2 => 'Интернет магазин SZ.UA',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/velotovary/velosupedy-dlya-vzroslyh/' => 
+  array (
+    0 => 'Велосипеды - купить велосипед в Киеве и по Украине, выгодная цена на велотовары в интернет магазине SZ.UA',
+    1 => '',
+    2 => 'Велосипеды',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/turuzm/ryukzaku--chehly-u--nakudku-dlya-ryukzakov/brend/deuter/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Рюкзаки Deuter',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/turuzm/kempungovaya-mebely/tip/stulja/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Стулья для пикника',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/turuzm/palatku/vmestimost/dvuhmestnye/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Палатки двухместные',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/turuzm/turustucheskue-kovruku/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Карематы',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/turuzm/spalynye-meshku/sezon/1184059448/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Спальный мешок зимний',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/tovary-dlya-detej/sport-i-aktivnyj-otdyh/sredstva-peredvizheniya/chudomobili-hodunki-kachalki/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Толокар',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/turuzm/fonaru/tip/kempingovyj/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Кемпинговые фонари',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Спорт и хобби',
+    3 => '',
+    4 => '',
+  ),
+  '//sz.ua/sport/turuzm/palatku/vmestimost/shestimestnye/' => 
+  array (
+    0 => '',
+    1 => '',
+    2 => 'Шестиместные палатки',
+    3 => '',
+    4 => '',
+  ),
+);
